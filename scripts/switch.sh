88f5b0a83b8e3e9c91122d19418313cf6c52cc73
sumniy <sumniy94@gmail.com>
2020-05-20 05:20:20 +0900

    스위치 스크립트 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,6 +11,6 @@ function switch_proxy() {
     echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-    echo "> 엔진엑스 Reload"
+    echo "> 엔진엑스 Reload:$IDLE_PORT"
     sudo service nginx reload
 }
