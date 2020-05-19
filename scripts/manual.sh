854367503c777a9f0f491fb87caea3d7cdbc780a
sumniy <sumniy94@gmail.com>
2020-05-20 05:40:38 +0900

    매뉴얼 스크립트 작성

--- scripts/manual.sh
+++ scripts/manual.sh
@@ -27,3 +27,32 @@ else
   kill -15 ${PID_OF_8082}
   sleep 5
 fi
+
+
+
+REPOSITORY=/home/ec2-user/app/step3
+
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+echo "> $JAR_NAME 를 profile=real1, real2 으로 실행합니다."
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real1.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real1 \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 && nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real2.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real2 \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
